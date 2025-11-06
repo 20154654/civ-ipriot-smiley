c2a8ba4ae96017b16f118023a8656a98fe6ca5af
20154654 <20154654@tafe.wa.edu.au>
2025-11-06 10:03:59 +0800

    Add files via upload
    uploaded Sad Smiley screenshot

---
+++ screenshots/main.py
@@ -0,0 +1,30 @@
+"""Demonstrates the use of the Smiley class and its subclasses.
+If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulator), you can use the real SenseHAT class instead of the mock SenseHAT class.
+That is, delete the sense_hat.py file that is included in this bundle."""
+
+import time
+
+from happy import Happy
+from sad import Sad
+
+def main():
+    happy_smiley = Happy()
+    happy_smiley.show()
+    time.sleep(1)
+    happy_smiley.blink()
+
+    sad_smiley = Sad()
+    sad_smiley.show()
+    time.sleep(2)
+    sad_smiley.blink()
+
+
+
+if __name__ == '__main__':
+    ############################################################
+    # Uncomment the lines below only if you have multi-processing issues
+    # from multiprocessing import freeze_support
+    # freeze_support()
+    ############################################################
+    main()
+
